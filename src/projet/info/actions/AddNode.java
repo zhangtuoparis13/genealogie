ebfd6636ebce0064f65e5bbb05fc85a0b6de1bc1
Timothée Oeuvray <oeuvray.timothee@gmail.com>
2014-06-13 13:59:04 +0200

    addNode merge

--- src/projet/info/actions/AddNode.java
+++ src/projet/info/actions/AddNode.java
@@ -24,14 +24,40 @@ public class AddNode implements CommandLine.ICommand {
 			int i = 1;
 			while (i < v.size()) {
 				final String arg = ((v.get(i) instanceof Double) ? new StringBuilder().append(""+(((Double) v.get(i)).intValue())).toString() : v.get(i).toString());
-				myDisplay.asyncExec(new Runnable() {
-					public void run() {
-						HandlingBinaryTrees grapher = new HandlingBinaryTrees();
-						grapher.cleanColor();
-						grapher.addNode(arg);
-						grapher.showGraph();
+				// Nouvelle fonction : si l'on tape "*<chiffre>", on créé plusieurs nodes prénommés
+				// C'est demandé par Timothée
+				if(arg.charAt(0)=='x') {
+					try {
+						// size contient le nombre de nodes désirés
+						int size = Integer.parseInt(arg.substring(1));
+						for(int tim=0;tim<size;tim++) {
+							myDisplay.asyncExec(new Runnable() {
+								public void run() {
+									HandlingBinaryTrees grapher = new HandlingBinaryTrees();
+									grapher.addNode();
+									grapher.showGraph();
+								}
+							});
+						}
+					} catch (Exception e) {
+						// C'est un nom normal !
+						myDisplay.asyncExec(new Runnable() {
+							public void run() {
+								HandlingBinaryTrees grapher = new HandlingBinaryTrees();
+								grapher.addNode(arg);
+								grapher.showGraph();
+							}
+						});
 					}
-				});
+				} else {
+					myDisplay.asyncExec(new Runnable() {
+						public void run() {
+							HandlingBinaryTrees grapher = new HandlingBinaryTrees();
+							grapher.addNode(arg);
+							grapher.showGraph();
+						}
+					});
+				}
 				++i;
 			}
 		}
