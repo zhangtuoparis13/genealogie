138c9602cf7f486c12d7e2a28a42aa46e47422cb
Timothée Oeuvray <oeuvray.timothee@gmail.com>
2014-06-13 13:56:08 +0200

    loadData merge

--- src/projet/info/actions/LoadData.java
+++ src/projet/info/actions/LoadData.java
@@ -11,8 +11,8 @@ import org.w3c.dom.Document;
 public class LoadData implements CommandLine.ICommand {
 	@Override
 	public boolean doIt(Vector v) {
+		String arg=null;
 		if (v.size() > 1) {
-			String arg=null;
 			System.out.println("Chargement du fichier...");
 			// Si l'utilisateur oublie le XML, le rajouter
 			if(!v.get(1).toString().substring(v.get(1).toString().length()-4,v.get(1).toString().length()).equalsIgnoreCase(".xml")) {
@@ -23,33 +23,33 @@ public class LoadData implements CommandLine.ICommand {
 			} else {
 				arg=v.get(1).toString();
 			}
-			File XMLFile = new File(arg);
-			if (XMLFile.exists()) {
-				try {
-					final Document FinalXML = DocumentBuilderFactory
-							.newInstance().newDocumentBuilder().parse(XMLFile);
-					System.out.println("Chargement du graph...");
-					Display myDisplay = HandlingBinaryTrees.getDisplay();
-					myDisplay.asyncExec(new Runnable() {
-						public void run() {
-							HandlingBinaryTrees grapher = new HandlingBinaryTrees();
-							grapher.cleanColor();
-							grapher.loadData(FinalXML);
-							grapher.showGraph();
-						}
-					});
-				} catch (Exception e) {
-					System.out.println("Erreur " + v.elementAt(0).toString()
-							+ " : Impossible de parser " + arg
-							+ " !");
-				}
-			} else {
+		} else {
+			System.out.println("Warning "+v.get(0)+" : Pas de fichier précisé");
+			System.out.println("Chargement de temp.xml...");
+			arg="temp.xml";
+		}
+		File XMLFile = new File(arg);
+		if (XMLFile.exists()) {
+			try {
+				final Document FinalXML = DocumentBuilderFactory
+						.newInstance().newDocumentBuilder().parse(XMLFile);
+				System.out.println("Chargement du graph...");
+				Display myDisplay = HandlingBinaryTrees.getDisplay();
+				myDisplay.asyncExec(new Runnable() {
+					public void run() {
+						HandlingBinaryTrees grapher = new HandlingBinaryTrees();
+						grapher.loadData(FinalXML);
+						grapher.showGraph();
+					}
+				});
+			} catch (Exception e) {
 				System.out.println("Erreur " + v.elementAt(0).toString()
-						+ " : Le fichier " + arg + " est inexistant");
+						+ " : Impossible de parser " + arg
+						+ " !");
 			}
 		} else {
 			System.out.println("Erreur " + v.elementAt(0).toString()
-					+ " : Pas assez d'arguments !");
+					+ " : Le fichier " + arg + " est inexistant");
 		}
 		return true;
 	}
