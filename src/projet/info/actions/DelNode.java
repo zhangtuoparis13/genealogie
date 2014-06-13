e0ef002dc3fca421b84166f05dba5117cb154931
Timothée Oeuvray <oeuvray.timothee@gmail.com>
2014-06-13 13:57:57 +0200

    delNode merge

--- src/projet/info/actions/DelNode.java
+++ src/projet/info/actions/DelNode.java
@@ -22,15 +22,41 @@ public class DelNode implements CommandLine.ICommand {
 		} else {
 			int i = 1;
 			while (i < v.size()) {
-				final String arg = ((v.get(i) instanceof Double) ? new StringBuilder().append(""+(((Double) v.get(i)).intValue())).toString() : v.get(i).toString());;
-				myDisplay.asyncExec(new Runnable() {
-					public void run() {
-						HandlingBinaryTrees grapher = new HandlingBinaryTrees();
-						grapher.cleanColor();
-						grapher.delNode(arg);
-						grapher.showGraph();
+				final String arg = ((v.get(i) instanceof Double) ? new StringBuilder().append(""+(((Double) v.get(i)).intValue())).toString() : v.get(i).toString());
+				// Nouvelle fonction : si l'on tape "*<chiffre>", on créé plusieurs nodes prénommés
+				// C'est demandé par Timothée
+				if(arg.charAt(0)=='x') {
+					try {
+						// size contient le nombre de nodes désirés
+						int size = Integer.parseInt(arg.substring(1));
+						for(int tim=0;tim<size;tim++) {
+							myDisplay.asyncExec(new Runnable() {
+								public void run() {
+									HandlingBinaryTrees grapher = new HandlingBinaryTrees();
+									grapher.delNode();
+									grapher.showGraph();
+								}
+							});
+						}
+					} catch (Exception e) {
+						// C'est un nom normal !
+						myDisplay.asyncExec(new Runnable() {
+							public void run() {
+								HandlingBinaryTrees grapher = new HandlingBinaryTrees();
+								grapher.delNode(arg);
+								grapher.showGraph();
+							}
+						});
 					}
-				});
+				} else {
+					myDisplay.asyncExec(new Runnable() {
+						public void run() {
+							HandlingBinaryTrees grapher = new HandlingBinaryTrees();
+							grapher.delNode(arg);
+							grapher.showGraph();
+						}
+					});
+				}
 				++i;
 			}
 		}
